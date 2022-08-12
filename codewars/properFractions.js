262ecb09762fcbd73129aeb4c049989b8a66f34c
maloded <damatomxxx@gmail.com>
2022-08-12 03:17:39 +0300

    update proper fractions

--- codewars/properFractions.js
+++ codewars/properFractions.js
@@ -1,31 +1,24 @@
 function properFractions(n){
-    // let counter = 0;
-    // for (let i = 1; i < n; i++) {
-    //     for (let j = 2; j < i+1; j++) {
-    //         if (n%j === 0 && i%j === 0) {
-    //             counter++;
-    //         }
-    //     }
-    // }
-    // return n-counter-1;
     let counter = 0;
     const arr = [];
 
-    for (let i = 2; i < n; i++) {
+    const b = n/3+1;
+    for (let i = 2; i < b; i++) {
         if (n % i === 0) {
             arr.push(i);
         }
     }
+    console.log(arr);
     const length = arr.length;
-    for (let i = n+1; i < n+n; i++) {
-        // if (arr.filter(e => i%e === 0).length === 0) {
-        //     counter++;
-        // }
+    const m = n+n;
+    for (let i = n+1; i < m; i++) {
         let count = 0;
-        for (let j = 0; j < arr.length; j++) {
+        for (let j = 0; j < length; j++) {
 
             if (i%arr[j] !== 0) {
                 count++;
+            } else {
+                break;
             }
         }
         if (count === length) {
@@ -35,8 +28,10 @@ function properFractions(n){
     return counter;
 }
 
-console.log(properFractions(1),0);
-console.log(properFractions(2),1);
-console.log(properFractions(5),4);
-console.log(properFractions(15),8);
+
+// console.log(properFractions(1),0);
+// console.log(properFractions(2),1);
+// console.log(properFractions(5),4);
+// console.log(properFractions(15),8);
 console.log(properFractions(25),20);
+// console.log(properFractions(253872), 80640);
