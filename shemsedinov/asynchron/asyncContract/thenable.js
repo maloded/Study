7cd1430fe212887a18f289d79cf01dbcf80b22fb
maloded <damatomxxx@gmail.com>
2022-11-15 14:33:53 +0200

    Add counter

---
+++ shemsedinov/asynchron/asyncContract/thenable.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+class Thenable {
+    constructor() {
+        this.thenHandler = null;
+        this.next = null;
+    }
+
+    then(fn) {
+        this.fn = fn;
+        const next = new Thenable;
+        this.next = next;
+        return next;
+    }
+
+    resolve(value) {
+        const fn = this.fn;
+        if (fn) {
+            const next = fn(value);
+            if (next) {
+                next.then(value => {
+                    this.next.resolve(value);
+                });
+            }
+        }
+    }
+}
+
+
+// Usage 
+
+const readFile = filename => {
+    const thenable = new Thenable();
+    fs.readFile(filename, 'utf8', (err, data) => {
+        if (err) throw err;
+        thenable.resolve(data);
+    });
+    return thenable;
+};
+
+(async () => {
+    const file1 = await readFile('../callback/emulate.js');
+    console.dir({ length: file1.length });
+})();
+
